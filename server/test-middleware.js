a146d87b9355b7008398f4f7c4f3a29efcefd3bf
VishK98 <90708676+VishK98@users.noreply.github.com>
2025-08-06 21:44:08 +0530

    employee done

--- server/test-middleware.js
+++
@@ -1,4 +0,0 @@
-const { authenticate } = require('./src/middlewares/auth.middleware');
-
-console.log('Middleware imported successfully:', typeof authenticate);
-console.log('Is function:', typeof authenticate === 'function'); 
